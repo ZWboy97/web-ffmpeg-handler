962ea001f9f1ae5f80cb357dd88487b3a6c4c577
JiaChang.Li <1769264507@qq.com>
2019-09-19 18:37:18 +0800

    ADD: 添加CORS中间件,支持浏览器跨域请求

--- src/server.js
+++ src/server.js
@@ -2,11 +2,13 @@ const Koa = require('koa');
 const router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 const apirouter = require('./api/route/index');
+const cors = require('./middleware/cors');
 
 const app = new Koa();
 
 app.use(bodyParser());
 app.use(apirouter.routes());
+app.use(cors);
 
 app.listen(3000)
 console.log('[web-ffmpeg-handler] is start at port 3000')
